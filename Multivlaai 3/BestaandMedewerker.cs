046141511096d466a0ec73b691209491e2128b3d
TJknigge <tjknigge1960@kpnmail.nl>
2021-10-12 19:49:03 +0200

    Add project files.

---
+++ Multivlaai 3/BestaandMedewerker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multivlaai_3
+{
+    class BestaandMedewerker : Persoongegevens
+    {
+        public string Nm;
+        public string Achter;
+        public string Functie;
+        public string Str;
+        public int NrHuis;
+        public string Postcd;
+        public string Woonpl;
+        public int Telefoon;
+        public string Geboort;
+
+
+        public BestaandMedewerker()
+        {
+
+
+            string[] Nm = { "Jan", "Piet", "Margriet", "Linda", "Gert", "Christien" };
+            string[] Achter = { "Groot", "Jansen", "Talens", "Hendriks", "Warnar", "van Gaal" };
+            string[] Functie = { "Inkoper", "verkoper", "inkoper", "verkoper", "manager", "verkoper" };
+            string[] Str = { "Tromplaan", "Tulp", "Bachplein", "Sationstraat", "De Meerloet", "Steenstraat" };
+            string[] NrHuis = { "1", "10", "4", "8", "12", "7" };
+            string[] Postcd = { "5010 KL", "2044 AC", "7557 DE", "6543 HJ", "2356 OP", "4758 TR" };
+            string[] Woonpl = { "Stijn", "Emmen", "Tiel", "Hoorn", "Meppel", "Goes" };
+            string[] Telefoon = { "0509658476", "0304569874", "0381254783", "0765412847", "0601472584", "0247856982" };
+            string[] Geboort = { "10-02-70", "02-08-1997", "07-11-1975", "09-12-1980", "25-04-1987", "19-06-2000" };
+
+            /*     
+                 
+                Console.WriteLine("naam:    \t |  achternaam:    \t |  functie:    \t |  strtaat:    \t |  huisnr:    \t |  postcode:    \t |  woonplaats:    \t |  telefoonnr:    \t |  geboortedatum: ");
+                for (int i = 0; i < Nm.Length; i++)
+                {
+                    Console.WriteLine("  {0}  \t |    {1}  \t |    {2}  \t |    {3}  \t |    {4}  \t |    {5}  \t |    {6}  \t |    {7}  \t |    {8}  ", Nm[i], Achter[i], Functie[i], Str[i], NrHuis[i], Postcd[i], Woonpl[i], Telefoon[i], Geboort[i]);
+                }*/
+
+        }
+        public static void Werker2(List<BestaandMedewerker> D)
+        {
+            Console.WriteLine("naam:    \t |  achternaam:    \t |  strtaat:    \t |  huisnr:    \t |  postcode:    \t |  woonplaats:    \t |  telefoonnr:    \t |  geboortedatum:    \t |  leeftijd: ");
+            foreach (var werker2 in D)
+            {
+                Console.WriteLine("werker2: {0}     \t |    {1}          \t |    {2}  \t |    {3}      \t |    {4}        \t |    {5}  \t         |    {6}  \t         |    {7}             \t |    {8} ", werker2.Nm, werker2.Achter, werker2.Str, werker2.NrHuis, werker2.Postcd, werker2.Woonpl, werker2.Telefoonnr, werker2.Geboort);
+            }
+        }
+
+    }
+}
---
+++ Multivlaai 3/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Multivlaai_3
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welkom in de Multivlaai omgeving");
+            Console.WriteLine("Bent u een klant voer 1 in, bent u een medewerker voer dan 2 in, om vlaai's in te voeren 3, alles tonen voer dan 4 in");
+            var Keuze = int.Parse(Console.ReadLine());
+
+            var klanten = new List<Klant>();
+            var medewerker = new List<Medewerker>();
+            var bestaklnt = new List<BestaandKlant>();
+            var bestwerk = new List<BestaandMedewerker>();
+            var verkoop = new List<Vlaai>();
+
+
+            if (Keuze == 1)
+            {
+                Console.WriteLine("Waar mee kan ik u helpen? ");
+
+                var doorgaan = true;
+                Console.WriteLine("invoeren klant gegevens");
+
+                while (doorgaan)
+                {
+                    var klant = new Klant();
+
+                    klanten.Add(klant);
+
+                    Console.Write("wilt u door gaan als volgende klant y/n?");
+                    if (Console.ReadLine() == "n")
+                    {
+                        doorgaan = false;
+                    }
+                }
+            }
+            else if (Keuze == 2)
+            {
+                Console.WriteLine("U kunt inlogen");
+
+                var doorgaan = true;
+                Console.WriteLine("invoeren medewerker gegevens");
+
+                while (doorgaan)
+                {
+                    var werker = new Medewerker();
+                    medewerker.Add(werker);
+
+                    Console.Write("wilt u door gaan met volgende medewerker y/n?");
+                    if (Console.ReadLine() == "n")
+                    {
+                        doorgaan = false;
+                    }
+                }
+            }
+            else if (Keuze == 3)
+            {
+                Console.WriteLine("Soorten Vlaai");
+
+                var doorgaan = true;
+
+
+                while (doorgaan)
+                {
+                    var verkocht = new Vlaai();
+                    verkoop.Add(verkocht);
+
+
+                    Console.Write("wilt u door gaan  y/n?");
+                    if (Console.ReadLine() == "n")
+                    {
+                        doorgaan = false;
+                    }
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Gegevens");
+
+                var doorgaan = true;
+
+
+                while (doorgaan)
+                {
+                    
+                    var bestaan = new BestaandKlant();
+                    bestaklnt.Add(bestaan);
+
+                    var bestmedew = new BestaandMedewerker();
+                    bestwerk.Add(bestmedew);
+
+                    Console.Write("wilt u door gaan  y/n?");
+                    if (Console.ReadLine() == "n")
+                    {
+                        doorgaan = false;
+                    }
+                }
+            }
+
+
+            Klant.Klant1(klanten);
+            BestaandKlant.Bestaan(bestaklnt);
+
+            Medewerker.Werker1(medewerker);
+            BestaandMedewerker.Werker2(bestwerk);
+            Vlaai.Toon(verkoop);
+        }
+
+    }      
+}
